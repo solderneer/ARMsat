f4733d42e937071cf078e22f49710496dfb20a3f
solderneer <sudhar393@gmail.com>
2017-03-08 08:46:02 +0800

    compartmentalised code

--- code/electron-app/src/app.js
+++ code/electron-app/src/app.js
@@ -6,9 +6,8 @@ import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
 import env from './env';
-//var SerialPort = require("serialport");
-
-//import { greet } from './hello_world/hello_world'
+var SerialPort = require("serialport");
+import { date, time } from './hello_world/datetime'
 
 console.log('Loaded environment variables:', env);
 
@@ -18,68 +17,18 @@ var appDir = jetpack.cwd(app.getAppPath());
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
 //user code starts here
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth();
-var yyyy = today.getFullYear();
-var hour = today.getHours();
-var minutes = today.getMinutes();
-var seconds = today.getSeconds();
-var day;
-
-if(dd < 10){
-    dd = '0' + dd;
-}
-
-if(mm < 10){
-    mm = '0' + mm;
-}
-
-if (hour < 12){
-    day = 'AM';
-}
-else {
-    day = 'PM'
-}
-
-if (hour > 12){
-    hour = hour - 12;
-}
-
-if (hour < 10){
-    hour = '0' + hour;
-}
-
-if (hour == '00'){
-    hour = '12';
-}
-
-if(minutes < 10){
-    minutes = '0' + minutes;
-}
-
-if(seconds < 10){
-    seconds = '0' + seconds;
-}
-
-today = dd + '/' + mm + '/' + yyyy;
-var time = hour + ':' + minutes + ':' + seconds + ' ' + day;
- 
- /*SerialPort.list(function(err ,ports) {
+ SerialPort.list(function(err ,ports) {
     ports.forEach(function(port) {
+        document.getElementById('COM-port').innerHTML = document.getElementById('COM-port').innerHTML + "<option>" + port.comName + "</option>";
         console.log(port.comName);
         console.log(port.pnpId);
         console.log(port.manufacturer);
     });
- });*/
+ });
 
 document.addEventListener('DOMContentLoaded', function () {
-    //document.getElementById('greet').innerHTML = greet();
     document.getElementById('platform-info').innerHTML = os.platform();
     console.log('I am actually here');
-    console.log(mm);
-    console.log(today);
-    console.log(time);
-    document.getElementById('date').innerHTML = today;
-    document.getElementById('time').innerHTML = time;
+    document.getElementById('date').innerHTML = date();
+    document.getElementById('time').innerHTML = time();
 });
