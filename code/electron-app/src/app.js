447a551b57243aee8abab2d31a341295c527f769
solderneer <sudhar393@gmail.com>
2017-03-08 19:51:28 +0800

    added camera scanning, camera videofeed, consolelog system and buttonclick event

--- code/electron-app/src/app.js
+++ code/electron-app/src/app.js
@@ -7,7 +7,7 @@ import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
 import env from './env';
 var SerialPort = require("serialport");
-import { date, time } from './hello_world/datetime'
+import { date, time, consoleLog, listMediaDevices } from './custom_modules/utility'
 
 console.log('Loaded environment variables:', env);
 
@@ -16,19 +16,38 @@ var appDir = jetpack.cwd(app.getAppPath());
 
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
-//user code starts here
- SerialPort.list(function(err ,ports) {
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('platform-info').innerHTML = os.platform();
+
+    //setting initial time/date
+    document.getElementById('date').innerHTML = date();
+    document.getElementById('time').innerHTML = time();
+
+    //listing available serial ports
+    SerialPort.list(function(err ,ports) {
     ports.forEach(function(port) {
         document.getElementById('COM-port').innerHTML = document.getElementById('COM-port').innerHTML + "<option>" + port.comName + "</option>";
         console.log(port.comName);
         console.log(port.pnpId);
         console.log(port.manufacturer);
-    });
- });
+            });
+        });
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('platform-info').innerHTML = os.platform();
-    console.log('I am actually here');
-    document.getElementById('date').innerHTML = date();
-    document.getElementById('time').innerHTML = time();
+    //listing available camera devices
+    listMediaDevices();
+
+    
+    document.getElementById("connect-btn").addEventListener('click', function (e) {
+        consoleLog('Connecting to camera...');
+        navigator.webkitGetUserMedia({video: true},
+        function(stream) {
+            document.getElementById('camera').src = URL.createObjectURL(stream);
+            consoleLog('Connected to camera')
+        },
+        function() {
+            consoleError('The selected camera could not be accessed');
+        });
+
+    });
 });
---
+++ code/electron-app/src/custom_modules/utility.js
@@ -0,0 +1,54 @@
+export var date = function () {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+
+    dd = dd < 10 ? "0" + dd: dd;
+    mm = mm < 10 ? "0" + mm: mm;
+
+    return (dd + '/' + mm + '/' + yyyy);
+}
+
+export var time = function () {
+    var today = new Date();
+    var hh = today.getHours();
+    var mm = today.getMinutes();
+    var ss = today.getSeconds();
+
+    return ((((hh == 0 ? 24 : hh) - 1) % 12 + 1) + ":" +
+            (mm < 10 ? "0" + mm: mm) + ":" +
+            (ss < 10 ? "0" + ss: ss) + " " +
+            ((hh == 0 || hh > 12) ? "PM" : "AM"));
+}
+
+export var consoleLog = function (x) {
+    document.getElementById('console').innerHTML = x;
+}
+
+export var consoleError = function (x) {
+    document.getElementById('console').innerHTML = "<span class='red'>" + x +"</span>"
+}
+
+export var listMediaDevices = function () {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+        console.log("enumerateDevices() not supported.");
+        return;
+    }
+    // List cameras and microphones.
+    navigator.mediaDevices.enumerateDevices().then(function(devices) {
+        devices.forEach(function(device) {
+            //console.log(device.kind + ": " + device.label + " id = " + device.deviceId);
+            if(device.kind == "videoinput") {
+                document.getElementById('Camera').innerHTML = document.getElementById('Camera').innerHTML + "<option>" + device.label + "</option>";
+            }
+        });
+    })
+    
+    .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+    });
+}
+
+
+
