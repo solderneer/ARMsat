95d662477491af9dfc6591def37dea50165eda18
solderneer <sudhar393@gmail.com>
2017-03-10 08:13:11 +0800

    comparmentalized code into functions

--- code/electron-app/src/app.js
+++ code/electron-app/src/app.js
@@ -7,7 +7,7 @@ import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
 import env from './env';
 var SerialPort = require("serialport");
-import { date, time, consoleLog, listMediaDevices, consoleError } from './custom_modules/utility'
+import { date, time, consoleLog, listMediaDevices, consoleError, listSerialDevices, connectRoutine } from './custom_modules/utility'
 
 console.log('Loaded environment variables:', env);
 
@@ -25,82 +25,16 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('time').innerHTML = time();
 
     //listing available serial ports
-    SerialPort.list(function(err ,ports) {
-    ports.forEach(function(port) {
-        document.getElementById('com-port').innerHTML = document.getElementById('com-port').innerHTML + "<option>" + port.comName + "</option>";
-        console.log(port.comName);
-        console.log(port.pnpId);
-        console.log(port.manufacturer);
-            });
-        });
+    listSerialDevices();
 
     //listing available camera devices
     listMediaDevices();
 
-    //Connect routine
     document.getElementById("connect-btn").addEventListener('click', function (e) {
-        var e = document.getElementById('camera-input');
-        var f = document.getElementById('baudrate');
-        var g = document.getElementById('com-port');
 
-        var selCam = e.options[e.selectedIndex].text;
-        var selBaudrate = f.options[f.selectedIndex].text;
-        var selCOMport = g.options[g.selectedIndex].text;
+        //Connection routine
+        connectRoutine();
 
-        console.log(selCam + ":" + selBaudrate + ":" + selCOMport);
 
-        document.getElementById('date').innerHTML = date();
-        document.getElementById('time').innerHTML = time();
-
-        //no camera selected error
-        if(selCam == "Camera"){
-            consoleError('No camera input selected');
-            return;
-        }
-        //no baudrate selected error
-        if(selBaudrate == "Baud Rate:"){
-            consoleError('No baudrate selected');
-            return;
-        }
-        /*no COM port selected error
-        if(selCOMport == "COM port"){
-            consoleError('No COM port selected');
-            return;
-        }*/
-
-        //Connect to camera
-        consoleLog('Connecting to camera...');
-        navigator.mediaDevices.enumerateDevices().then(function(devices) {
-            devices.forEach(function(device) {
-                
-                if (device.label == selCam) {
-                    var constraints = { deviceId: { exact: device.deviceId } };
-                    navigator.webkitGetUserMedia({video: constraints},
-                        function(stream) {
-                            document.getElementById('camera').src = URL.createObjectURL(stream);
-                            consoleLog('Connected to: ' + device.label)
-                        },
-                        function() {
-                            consoleError('The selected camera could not be accessed');
-                            break;
-                        }
-                    );
-
-                }
-            });
-        })
-
-        //Connect to serialport
-        var port = new SerialPort(selCOMport, { baudRate: selBaudrate}, function (err) {
-            if (err) {
-                return console.log('Error:', err.message);
-            }
-        });
-
-        document.getElementById('connect-btn').src = "assets/unlink.png";
-        document.getElementById('connect-btn').id = "disconnect-btn";
-        document.getElementsByClassName('right')[0].className = "lessright";
-        document.getElementsByClassName('lessright')[0].innerHTML = "Disconnect";
-
+        }); 
     });
-});
--- code/electron-app/src/custom_modules/utility.js
+++ code/electron-app/src/custom_modules/utility.js
@@ -1,3 +1,5 @@
+var SerialPort = require("serialport");
+
 export var date = function () {
     var today = new Date();
     var dd = today.getDate();
@@ -50,5 +52,78 @@ export var listMediaDevices = function () {
     });
 }
 
+export var listSerialDevices = function () {
+    SerialPort.list(function(err ,ports) {
+    ports.forEach(function(port) {
+        document.getElementById('com-port').innerHTML = document.getElementById('com-port').innerHTML + "<option>" + port.comName + "</option>";
+        console.log(port.comName);
+        console.log(port.pnpId);
+        console.log(port.manufacturer);
+            });
+        });
+}
 
+export var connectRoutine = function () {
+    /**
+     * Connect Routine
+     */
+        var e = document.getElementById('camera-input');
+        var f = document.getElementById('baudrate');
+        var g = document.getElementById('com-port');
 
+        var selCam = e.options[e.selectedIndex].text;
+        var selBaudrate = f.options[f.selectedIndex].text;
+        var selCOMport = g.options[g.selectedIndex].text;
+
+        console.log(selCam + ":" + selBaudrate + ":" + selCOMport);
+
+        document.getElementById('date').innerHTML = date();
+        document.getElementById('time').innerHTML = time();
+
+        //no camera selected error
+        if(selCam == "Camera"){
+            consoleError('No camera input selected');
+            return;
+        }
+        //no baudrate selected error
+        if(selBaudrate == "Baud Rate:"){
+            consoleError('No baudrate selected');
+            return;
+        }
+        /*no COM port selected error
+        if(selCOMport == "COM port"){
+            consoleError('No COM port selected');
+            return;
+        }*/
+
+        //Connect to camera
+        consoleLog('Connecting to camera...');
+        navigator.mediaDevices.enumerateDevices().then(function(devices) {
+            devices.forEach(function(device) {
+                
+                if (device.label == selCam) {
+                    var constraints = { deviceId: { exact: device.deviceId } };
+                    navigator.webkitGetUserMedia({video: constraints},
+                        function(stream) {
+                            document.getElementById('camera').src = URL.createObjectURL(stream);
+                            consoleLog('Connected to: ' + device.label)
+                        },
+                        function() {
+                            consoleError('The selected camera could not be accessed');
+                        }
+                    );
+                }
+            });
+        })
+
+        /*Connect to serialport
+        var port = new SerialPort(selCOMport, { baudRate: selBaudrate}, function (err) {
+            if (err) {
+                return console.log('Error:', err.message);
+            }
+        });*/
+
+        document.getElementById('connect-btn').src = "assets/unlink.png";
+        document.getElementsByClassName('right')[0].className = "lessright";
+        document.getElementsByClassName('lessright')[0].innerHTML = "Disconnect";
+}
